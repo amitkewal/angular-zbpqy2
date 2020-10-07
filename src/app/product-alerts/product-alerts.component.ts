7271a39fe5eb7545292f9d3d160b8f2b14b40a12
amitkewal <amit.iitr.cs@gmail.com>
2020-10-07 21:15:14 +0530

    2 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { Input } from "@angular/core";
 import { Output, EventEmitter } from "@angular/core";
 import { SwPush } from "@angular/service-worker";
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: "app-product-alerts",
@@ -11,16 +13,29 @@ import { SwPush } from "@angular/service-worker";
 export class ProductAlertsComponent implements OnInit {
   @Input() product;
   @Output() notify = new EventEmitter();
+  apiData: any;
   private readonly publicKey =
     "BLlFd7xr7xV3Hjce48ARU98KQ-hpJEblngRlMzMMn17__Vw_rH4WFlcqzJPktV3XRoUOeADnlMzmmoqJban5898";
 
-  constructor(readonly swPush: SwPush) {}
+  constructor(readonly swPush: SwPush,private http: HttpClient,) {}
 
   ngOnInit() {
     this.pushSubscription();
     this.swPush.messages.subscribe(message =>
       console.log("||||||||||", message)
     );
+     this.swPush.notificationClicks.subscribe(({ action, notification }) => {
+      window.open(notification.data.url);
+    });
+
+    // this.http.get('https://webpushtest-291012.firebaseapp.com/?87618=').subscribe(
+    //   (res: any) => {
+    //     this.apiData = res.data;
+    //   },
+    //   (err) => {
+    //     console.error(err);
+    //   }
+    // );
   }
 
   pushSubscription() {
